BUZDOLAPCI/FrogPilot

FrogPilot features - Automatic updates

selfdrive/frogpilot/frogpilot_process.py
@@ -1,5 +1,6 @@
 import datetime
 import http.client
+import os
 import socket
 import urllib.error
 import urllib.request
@@ -8,6 +9,7 @@ from cereal import log, messaging
 from openpilot.common.params import Params
 from openpilot.common.realtime import Priority, config_realtime_process
 from openpilot.common.time import system_time_valid
+from openpilot.system.hardware import HARDWARE
 
 from openpilot.selfdrive.frogpilot.controls.frogpilot_planner import FrogPilotPlanner
 from openpilot.selfdrive.frogpilot.controls.lib.frogpilot_functions import FrogPilotFunctions
@@ -22,9 +24,25 @@ def github_pinged(url="https://github.com", timeout=5):
   except (urllib.error.URLError, socket.timeout, http.client.RemoteDisconnected):
     return False
 
-def time_checks(deviceState, now, params, params_memory):
+def automatic_update_check(params):
+  update_available = params.get_bool("UpdaterFetchAvailable")
+  update_ready = params.get_bool("UpdateAvailable")
+  update_state_idle = params.get("UpdaterState", encoding='utf8') == "idle"
+
+  if update_ready:
+    HARDWARE.reboot()
+  elif update_available:
+    os.system("pkill -SIGHUP -f selfdrive.updated.updated")
+  elif update_state_idle:
+    os.system("pkill -SIGUSR1 -f selfdrive.updated.updated")
+
+def time_checks(automatic_updates, deviceState, now, params, params_memory):
   screen_off = deviceState.screenBrightnessPercent == 0
   wifi_connection = deviceState.networkType == WIFI
+
+  if screen_off and wifi_connection:
+    if automatic_updates:
+      automatic_update_check(params)
 
 def frogpilot_thread(frogpilot_toggles):
   config_realtime_process(5, Priority.CTRL_LOW)
@@ -59,7 +77,7 @@ def frogpilot_thread(frogpilot_toggles):
 
     if not started and now.second == 0 or not time_validated:
       if github_pinged():
-        time_checks(deviceState, now, params, params_memory)
+        time_checks(frogpilot_toggles.automatic_updates, deviceState, maps_downloaded, now, params, params_memory)
 
       if not time_validated:
         time_validated = system_time_valid()

foodbandlt/openpilot

selfdrive/frogpilot/frogpilot_process.py
@@ -1,5 +1,6 @@
 import datetime
 import http.client
+import os
 import socket
 import urllib.error
 import urllib.request
@@ -8,6 +9,7 @@ from cereal import log, messaging
 from openpilot.common.params import Params
 from openpilot.common.realtime import Priority, config_realtime_process
 from openpilot.common.time import system_time_valid
+from openpilot.system.hardware import HARDWARE
 
 from openpilot.selfdrive.frogpilot.controls.frogpilot_planner import FrogPilotPlanner
 from openpilot.selfdrive.frogpilot.controls.lib.frogpilot_functions import FrogPilotFunctions
@@ -22,9 +24,25 @@ def github_pinged(url="https://github.com", timeout=5):
   except (urllib.error.URLError, socket.timeout, http.client.RemoteDisconnected):
     return False
 
-def time_checks(deviceState, now, params, params_memory):
+def automatic_update_check(params):
+  update_available = params.get_bool("UpdaterFetchAvailable")
+  update_ready = params.get_bool("UpdateAvailable")
+  update_state_idle = params.get("UpdaterState", encoding='utf8') == "idle"
+
+  if update_ready:
+    HARDWARE.reboot()
+  elif update_available:
+    os.system("pkill -SIGHUP -f system.updated.updated")
+  elif update_state_idle:
+    os.system("pkill -SIGUSR1 -f system.updated.updated")
+
+def time_checks(automatic_updates, deviceState, now, params, params_memory):
   screen_off = deviceState.screenBrightnessPercent == 0
   wifi_connection = deviceState.networkType == WIFI
+
+  if screen_off and wifi_connection:
+    if automatic_updates:
+      automatic_update_check(params)
 
 def frogpilot_thread(frogpilot_toggles):
   config_realtime_process(5, Priority.CTRL_LOW)
@@ -65,7 +83,7 @@ def frogpilot_thread(frogpilot_toggles):
     if now.second == 0 or not time_validated:
       if not started:
         if github_pinged():
-          time_checks(deviceState, now, params, params_memory)
+          time_checks(frogpilot_toggles.automatic_updates, deviceState, maps_downloaded, now, params, params_memory)
 
       if not time_validated:
         time_validated = system_time_valid()